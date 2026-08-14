91ab6771448ec16ce8437babfccb3ceda22f2030
barjsamal-svg <barjsamal@svg.com>
2026-08-14 23:04:17 +0000

    Adjust SDK versions to 35 and fix workflow download logic (removed secrets)

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.samal.v2ray"
-    compileSdk = 37
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.samal.v2ray"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 743
         versionName = "2.3.3"
 
