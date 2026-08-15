a1dd9a7f130ec4c0b00ec1bc993245ec7f6500ae
barjsamal-svg <barj.samal@github.com>
2026-08-15 00:39:26 +0000

    Fix export crash with proper FileProvider authority and paths, add samal export string

--- V2rayNG/app/src/main/java/com/samal/v2ray/ui/main/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/samal/v2ray/ui/main/MainActivity.kt
@@ -131,7 +131,7 @@ class MainActivity : HelperBaseComponentActivity() {
         val htmlMsg = "<h6 align=\\\"center\\\"><span style=\\\"text-align: center;\\\"><h4 style=\\\"text-align:center;\\\"> <b><span style=\\\"color:aqua;\\\">🇮🇶 𝑰𝑸 𝑵𝑬𝑻 🇮🇶\\n\\n🌟محترفو الانترنت المجاني🌟"
         val samalFile = AngConfigManager.share2Samal(this, guid, htmlMsg, "2029-12-31")
         if (samalFile != null && samalFile.exists()) {
-            val uri = androidx.core.content.FileProvider.getUriForFile(this, "${packageName}.fileprovider", samalFile)
+            val uri = androidx.core.content.FileProvider.getUriForFile(this, "${packageName}.cache", samalFile)
             val intent = Intent(Intent.ACTION_SEND).apply {
                 type = "*/*"
                 putExtra(Intent.EXTRA_STREAM, uri)
